51745f0d5332cb2743ca594cad129fa33becc370
DJPretorius <20798136+DJPretorius@users.noreply.github.com>
2017-12-01 12:20:42 +0200

    Change Folder Layout

--- snap/Pool.java
+++
@@ -1,5 +0,0 @@
-package com.example.niel.snap;
-
-public class Pool extends Holder {
-
-}
--- snap/Suits.java
+++
@@ -1,9 +0,0 @@
-package com.example.niel.snap;
-
-/**
- *
- * @author Niel
- */
-public enum Suits {
-   Hearts,Diamonds, Clovers, Spades, Joker
-}
